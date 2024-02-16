c5eb6780c2643f1d6444cd4850f81af618741975
ridwanhanafi <ridwanacmad619@gmail.com>
2024-02-16 22:17:22 +0700

    Projek 1 bagian 5(A) grup list dan pagination

---
+++ src/Components/Filter/GroupList.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const GroupList = (props) => {
+    const {items, textProperty, valueProperty, selectedItem, onItemSelect} = props
+  return (
+    <ul className='list-group'>
+      {items.map(item => ( 
+        // items diubah menjadi item
+        <li
+        onClick={() => onItemSelect(item)}
+        key={item[valueProperty]}
+        className={item === selectedItem ? "list-group-item active" : "list-group-item"} //Dibaca : jika item dipilih atau diklik maka list grup akan active jika tidak maka akan biasa aja list grup nya
+        >
+            
+            {item[textProperty]} {/*ini untuk memanggil semua data atau itemnya */}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+GroupList.defaultProops = {
+  textProperty: "name",
+  valueProperty: "_id"
+}
+
+export default GroupList
---
+++ src/Components/Filter/Pagination.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import propTypes from 'prop-types';
+import _ from 'lodash';
+import { propTypes } from 'react-bootstrap/esm/Image';
+
+const Pagination = (props) => {
+  const {itemCount, pageSize, currentPage, onPageChange } = props //itemCount ini untuk menghitung data yang akan ditampilkan, contoh mau tampilkan data 100 dengan pembagian 10 10.
+  
+  const pageCount = Math.ceil(itemCount / pageSize) //variabel digunakan untuk menghitung data agar bisa memunculkan data sesuai dengan keinginan.
+  
+  if(pageCount === 1) return null; 
+  const pages = _.range(1, pageCount + 1) //untuk menghitung paginationnya jadi saat teken tombol next nya maka akan lanjut ke default 1 + 1 yaitu 2, dst.
+  return (
+    <nav>
+      <ul className="nav-item">
+        {pages.map(page => (
+          // logic li dibawah sama kayak logic di GroupList.jsx
+          <li key={page} className={page === currentPage ? "page-item active" : "page-item"}> 
+            <a className='page-link' onClick={() => onPageChange(page)}>
+                 {page}
+            </a> 
+            {/* dibaca: jalankan link a nya yang saat di klik akan menjalankan onPageChange nya */}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+Pagination.propTypes = {
+  itemCount: propTypes.number.isRequired,
+  pageSize: propTypes.number.isRequired,
+  currentPage: propTypes.number.isRequired,
+  onPageChange: propTypes.func.isRequired
+}
+
+export default Pagination
