591fa065cb1de68b8e6fa3653979f6ec96f4f40f
ridwanhanafi <ridwanacmad619@gmail.com>
2024-02-18 21:45:07 +0700

    Projek 1 bagian 5(A) grup list dan pagination

--- src/Components/Filter/Pagination.jsx
+++ src/Components/Filter/Pagination.jsx
@@ -8,7 +8,7 @@ const Pagination = (props) => {
   
   const pageCount = Math.ceil(itemCount / pageSize) //variabel digunakan untuk menghitung data agar bisa memunculkan data sesuai dengan keinginan.
   
-  if(pageCount === 1) return null; 
+  if(pageCount === 1) return null; //dibaca: jika atau saat web nya dibuka maka sudah pasti ke pagination 1 atau defaultnya.
   const pages = _.range(1, pageCount + 1) //untuk menghitung paginationnya jadi saat teken tombol next nya maka akan lanjut ke default 1 + 1 yaitu 2, dst.
   return (
     <nav>
@@ -28,6 +28,7 @@ const Pagination = (props) => {
 }
 
 Pagination.propTypes = {
+  // isRequired ini maksudnya adalah harus diisi.
   itemCount: propTypes.number.isRequired,
   pageSize: propTypes.number.isRequired,
   currentPage: propTypes.number.isRequired,
